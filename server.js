1782dd12fdf3c79785a44e7e0bb324e8b779ffa3
Ryan Gross <ryangrossmn@gmail.com>
2022-10-24 16:45:04 -0600

    add the server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http.createServer(function(request, response) {    
+   response.writeHead(200, {"Content-Type": "text/html"});
+   response.write("<h1>Hello, Node.js!</h1>");    
+   response.write("<h2>Ryan Gross - d31b426</h2> \
+      <p>Running node server on port 3062</p>");
+   response.end();
+}).listen(3000);
